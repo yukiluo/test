58b05b5ed9fee30cb7fc0cac44b8248831b94a55
BrLi <rainman59118@gmail.com>
2018-03-08 21:31:39 +0800

    comment off express-sessions control, add mysql node module

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
+var session = require('express-sessions');
 var routes = require('./routes/index');
 var order = require('./routes/order');
 var newOrder=require('./routes/newOrder');
@@ -21,10 +21,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.use(session({secret : 'session'}));
+// app.use(session({secret : 'session'}));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
